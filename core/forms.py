0b6d5973f3b686110b9c2f0ec05a6addb8b2698b
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-24 18:03:30 +0100

    ready to crispyforms

--- core/forms.py
+++ core/forms.py
@@ -1,19 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-field block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
-                                      'placeholder': 'Username'}))
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-field block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
-                                          'placeholder': 'Password'}))
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-field block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
-                                          'placeholder': 'Confirm Password'}))
-
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username', 'password1', 'password2', )
