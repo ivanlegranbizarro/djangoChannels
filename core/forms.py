ce6d494ce90fbb64957bd392b469518ce0c9ceaf
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-24 17:01:45 +0100

    first commit django channels chat

---
+++ core/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
+                                      'placeholder': 'Username'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
+                                          'placeholder': 'Password'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'block w-full p-3 my-2 text-gray-700 bg-gray-200 rounded-md',
+                                          'placeholder': 'Confirm Password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
+
+
+def frontpage(request):
+    return render(request, 'frontpage.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
