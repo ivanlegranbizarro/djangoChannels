0cf76bf5b23491332446aae39aa87c8f1c5beb5b
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-25 08:39:19 +0100

    anidado listado de rooms

--- room/views.py
+++ room/views.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .forms import RoomForm
+from .models import Room
 
 
+@login_required()
 def create_rooms(request):
+    rooms = Room.objects.all()
     form = RoomForm()
-    return render(request, 'create-rooms.html', {'form': form})
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = RoomForm()
+    return render(request, 'create-rooms.html', {'form': form, 'rooms': rooms})
